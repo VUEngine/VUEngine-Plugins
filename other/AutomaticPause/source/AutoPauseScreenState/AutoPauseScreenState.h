4e118cc66bf0db78bca1dd4dfbb41b9af3e32311
Christian Radke <chris@vr32.de>
2018-11-28 13:49:08 +0100

    Changed AutoPauseScreenState to singleton

--- other/AutomaticPause/source/AutoPauseScreenState/AutoPauseScreenState.h
+++ other/AutomaticPause/source/AutoPauseScreenState/AutoPauseScreenState.h
@@ -35,7 +35,7 @@
 //											CLASS'S DECLARATION
 //---------------------------------------------------------------------------------------------------------
 
-dynamic_singleton class AutoPauseScreenState : GameState
+singleton class AutoPauseScreenState : GameState
 {
 	static AutoPauseScreenState getInstance();
 	override void processUserInput(UserInput userInput);
