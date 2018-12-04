d4d5016435f5e3cf49118cbb5998cf38c24c18ac
jorgeche <jorgech3@gmail.com>
2018-12-04 17:57:49 -0500

    Implemented Platformer Camera Movement Manager plugin.

---
+++ cameras/Platformer/source/PlatformerCameraMovementManager/PlatformerCameraMovementManager.c
@@ -0,0 +1,375 @@
+/* VUEngine - Virtual Utopia Engine <http://vuengine.planetvb.com/>
+ * A universal game engine for the Nintendo Virtual Boy
+ *
+ * Copyright (C) 2007, 2018 by Jorge Eremiev <jorgech3@gmail.com> and Christian Radke <chris@vr32.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+ * NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+//---------------------------------------------------------------------------------------------------------
+//												INCLUDES
+//---------------------------------------------------------------------------------------------------------
+
+#include <GameEvents.h>
+#include <PlatformerCameraMovementManager.h>
+#include <Optics.h>
+#include <Camera.h>
+#include <MessageDispatcher.h>
+#include <Actor.h>
+#include <Game.h>
+#include <PhysicalWorld.h>
+#include <InverseBox.h>
+#include <EventManager.h>
+#include <Utilities.h>
+
+
+//---------------------------------------------------------------------------------------------------------
+//												CLASS'S METHODS
+//---------------------------------------------------------------------------------------------------------
+
+// class's constructor
+void PlatformerCameraMovementManager::constructor()
+{
+	// construct base object
+	Base::constructor();
+
+	this->positionFlag.x = 0;
+	this->positionFlag.y = 0;
+	this->positionFlag.z = 0;
+
+	this->cameraTrigger = NULL;
+	this->focusFunction = &PlatformerCameraMovementManager::doFocus;
+	this->previousFocusFunction = this->focusFunction;
+
+	this->camera = Camera::getInstance();
+
+	PlatformerCameraMovementManager::configure(this, kNoType, kNoLayer, kNoLayer, (PixelSize){8 * 8, 8 * 8, 8 * 8}, (Vector3D){0, 0, 0});
+
+	NM_ASSERT(this->camera, "PlatformerCameraMovementManager::constructor: null this->camera");
+}
+
+// class's destructor
+void PlatformerCameraMovementManager::destructor()
+{
+	// destroy base
+	Base::destructor();
+}
+
+// center world's this->camera in function of focus actor's position
+void PlatformerCameraMovementManager::focus(u32 checkIfFocusEntityIsMoving __attribute__ ((unused)))
+{
+	this->focusFunction(this, checkIfFocusEntityIsMoving, false);
+}
+
+bool PlatformerCameraMovementManager::doFocusWithNoEasing(u32 checkIfFocusEntityIsMoving __attribute__ ((unused)), u32 introFocusing __attribute__ ((unused)))
+{
+	Vector3D focusEntityPosition = Camera::getFocusEntityPosition(this->camera);
+	Direction direction = Entity::getDirection(Entity::safeCast(Camera::getFocusEntity(this->camera)));
+
+	Vector3D cameraPosition =
+	{
+		focusEntityPosition.x + direction.x * Camera::getFocusEntityPositionDisplacement(this->camera).x - __PIXELS_TO_METERS(__SCREEN_WIDTH / 2),
+		focusEntityPosition.y + Camera::getFocusEntityPositionDisplacement(this->camera).y - __PIXELS_TO_METERS(__SCREEN_HEIGHT / 2),
+		0
+	};
+
+	Camera::setPosition(this->camera, cameraPosition);
+
+	return true;
+}
+
+// center world's this->camera in function of focus actor's position
+bool PlatformerCameraMovementManager::dontFocus(u32 checkIfFocusEntityIsMoving __attribute__ ((unused)), u32 introFocusing __attribute__ ((unused)))
+{
+	return false;
+}
+
+// center world's this->camera in function of focus actor's position
+bool PlatformerCameraMovementManager::doFocus(u32 checkIfFocusEntityIsMoving __attribute__ ((unused)), u32 introFocusing __attribute__ ((unused)))
+{
+	// if focusEntity is defined
+	if(!Camera::getFocusEntity(this->camera))
+	{
+		return false;
+	}
+
+	Actor focusActor = Actor::safeCast(Camera::getFocusEntity(this->camera));
+
+	Direction direction = Entity::getDirection(focusActor);
+
+	Vector3D cameraNewPosition = Camera::getPosition(this->camera);
+
+	Vector3DFlag reachedTargetFlag = {true, true, true};
+
+	Vector3D focusEntityPosition = Camera::getFocusEntityPosition(this->camera);
+	Vector3D focusEntityPositionDisplacement = Camera::getFocusEntityPositionDisplacement(this->camera);
+
+	Vector3D position3D = Vector3D::getRelativeToCamera(focusEntityPosition);
+	PixelVector position2D = Vector3D::projectToPixelVector(position3D, 0);
+
+	Size stageSize = Camera::getStageSize(this->camera);
+
+	{
+		bool focusEntityOutOfBounds = (unsigned)(position2D.x - _cameraFrustum->x0 - SCREEN_WIDTH_REDUCTION) > (unsigned)(_cameraFrustum->x1 - _cameraFrustum->x0 - SCREEN_WIDTH_REDUCTION);
+
+		if(this->positionFlag.x | focusEntityOutOfBounds)
+		{
+			// calculate the target position
+			fix10_6 horizontalPosition = cameraNewPosition.x;
+			fix10_6 horizontalTarget = focusEntityPosition.x + direction.x * focusEntityPositionDisplacement.x - __PIXELS_TO_METERS(__SCREEN_WIDTH / 2);
+
+			fix10_6 easingDisplacement = __PIXELS_TO_METERS(7);
+
+			if(introFocusing)
+			{
+				easingDisplacement = __PIXELS_TO_METERS(1);
+			}
+
+			reachedTargetFlag.x = false;
+
+			if(horizontalPosition + easingDisplacement < horizontalTarget)
+			{
+				cameraNewPosition.x += easingDisplacement;
+			}
+			else if(horizontalPosition - easingDisplacement > horizontalTarget)
+			{
+				cameraNewPosition.x -= easingDisplacement;
+			}
+			else
+			{
+				cameraNewPosition.x = horizontalTarget;
+				reachedTargetFlag.x = true;
+			}
+
+			if(0 > cameraNewPosition.x)
+			{
+				reachedTargetFlag.x = true;
+			}
+			else if(stageSize.x < cameraNewPosition.x + __PIXELS_TO_METERS(__SCREEN_WIDTH))
+			{
+				reachedTargetFlag.x = true;
+			}
+		}
+	}
+
+	{
+		bool focusEntityOutOfBounds = position2D.y > _cameraFrustum->y1 - SCREEN_HEIGHT_REDUCTION || position2D.y < _cameraFrustum->y0 + SCREEN_HEIGHT_REDUCTION / 4;
+
+		if(this->positionFlag.y | focusEntityOutOfBounds)
+		{
+			// calculate the target position
+			fix10_6 verticalPosition = cameraNewPosition.y;
+			fix10_6 verticalTarget = focusEntityPosition.y + focusEntityPositionDisplacement.y - __PIXELS_TO_METERS(__SCREEN_HEIGHT / 2);
+
+			fix10_6 downEasingDisplacement = __PIXELS_TO_METERS(3);
+			fix10_6 upEasingDisplacement = __PIXELS_TO_METERS(3);
+
+			if(introFocusing)
+			{
+				downEasingDisplacement = __PIXELS_TO_METERS(1);
+				upEasingDisplacement = __PIXELS_TO_METERS(1);
+			}
+			else
+			{
+				Velocity velocity = Actor::getVelocity(focusActor);
+
+				if(0 < velocity.y)
+				{
+					downEasingDisplacement = __PIXELS_TO_METERS(8);
+				}
+			}
+
+			reachedTargetFlag.y = false;
+
+			if(focusEntityOutOfBounds)
+			{
+				this->positionFlag.y = true;
+			}
+
+			if(verticalPosition + downEasingDisplacement < verticalTarget)
+			{
+				cameraNewPosition.y += downEasingDisplacement;
+			}
+			else if(verticalPosition - upEasingDisplacement > verticalTarget)
+			{
+				cameraNewPosition.y -= upEasingDisplacement;
+			}
+			else
+			{
+				cameraNewPosition.y = verticalTarget;
+				this->positionFlag.y = false;
+				reachedTargetFlag.y = true;
+			}
+
+			if(0 > cameraNewPosition.y)
+			{
+				reachedTargetFlag.y = true;
+			}
+			else if(stageSize.y < cameraNewPosition.y + __PIXELS_TO_METERS(__SCREEN_HEIGHT))
+			{
+				reachedTargetFlag.y = true;
+			}
+		}
+	}
+
+	Camera::setPosition(this->camera, cameraNewPosition);
+
+	if(reachedTargetFlag.x && reachedTargetFlag.y)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+// center world's this->camera in function of focus actor's position
+bool PlatformerCameraMovementManager::doFocusAndAlertWhenTargetReached(u32 checkIfFocusEntityIsMoving __attribute__ ((unused)), u32 introFocusing __attribute__ ((unused)))
+{
+	if(PlatformerCameraMovementManager::doFocus(this, checkIfFocusEntityIsMoving, true))
+	{
+		Object::fireEvent(EventManager::getInstance(), kEventScreenFocused);
+
+		return true;
+	}
+
+	return false;
+}
+
+void PlatformerCameraMovementManager::setPositionFlag(Vector3DFlag positionFlag)
+{
+	this->positionFlag = positionFlag;
+}
+
+Vector3DFlag PlatformerCameraMovementManager::getPositionFlag()
+{
+	return this->positionFlag;
+}
+
+void PlatformerCameraMovementManager::enable()
+{
+	this->focusFunction = this->previousFocusFunction;
+	this->previousFocusFunction = this->focusFunction;
+}
+
+void PlatformerCameraMovementManager::disable()
+{
+	if(&PlatformerCameraMovementManager::dontFocus != this->focusFunction)
+	{
+		this->previousFocusFunction = this->focusFunction;
+	}
+
+	this->focusFunction = &PlatformerCameraMovementManager::dontFocus;
+}
+
+void PlatformerCameraMovementManager::enableFocusEasing()
+{
+	this->focusFunction = &PlatformerCameraMovementManager::doFocus;
+	this->previousFocusFunction = this->focusFunction;
+}
+
+void PlatformerCameraMovementManager::disableFocusEasing()
+{
+	this->focusFunction = &PlatformerCameraMovementManager::doFocusWithNoEasing;
+	this->previousFocusFunction = this->focusFunction;
+}
+
+void PlatformerCameraMovementManager::alertWhenTargetFocused()
+{
+	if(&PlatformerCameraMovementManager::doFocusAndAlertWhenTargetReached != this->focusFunction)
+	{
+		this->previousFocusFunction = this->focusFunction;
+	}
+
+	this->focusFunction = &PlatformerCameraMovementManager::doFocusAndAlertWhenTargetReached;
+}
+
+void PlatformerCameraMovementManager::dontAlertWhenTargetFocused()
+{
+	this->focusFunction = this->previousFocusFunction;
+}
+
+void PlatformerCameraMovementManager::focusGameEntitySet(Entity focusEntity)
+{
+	if(focusEntity)
+	{
+		this->cameraTrigger = Entity::addChildEntity(focusEntity, (EntityDefinition*)&this->platformerCameraTriggerEntityDefinition, 0, NULL, &this->boundingBoxDisplacement, NULL);
+	}
+}
+
+void PlatformerCameraMovementManager::lockMovement(u8 axisToLockUp, bool locked)
+{
+	if(!isDeleted(this->cameraTrigger))
+	{
+		PlatformerCameraTriggerEntity::lockMovement(this->cameraTrigger, axisToLockUp, locked);
+	}
+}
+
+void PlatformerCameraMovementManager::configure(u32 focusEntityInGameType, u32 focusEntityLayer, u32 cameraTriggerLayer, PixelSize boundingBoxSize, Vector3D boundingBoxDisplacement)
+{
+	this->boundingBoxDisplacement = boundingBoxDisplacement;
+
+	this->platformerCameraTriggerEntityShapesDefinition[0] = (ShapeDefinition)
+	{
+		// shape
+		__TYPE(InverseBox),
+
+		// size (x, y, z)
+		boundingBoxSize,
+
+		// displacement (x, y, z, p)
+		{0, 0, 0, 0},
+
+		// rotation (x, y, z)
+		{0, 0, 0},
+
+		// scale (x, y, z)
+		{0, 0, 0},
+
+		/// if true this shape checks for collisions against other shapes
+		true,
+
+		/// layers in which I live
+		cameraTriggerLayer,
+
+		/// layers to ignore when checking for collisions
+		~(focusEntityLayer),
+	};
+
+	this->platformerCameraTriggerEntityShapesDefinition[1] = (ShapeDefinition) {NULL, {0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0}, {0, 0, 0}, false, kNoLayer, kNoLayer};
+
+	this->platformerCameraTriggerEntityDefinition = (PlatformerCameraTriggerEntityDefinition)
+	{
+		// class allocator
+		__TYPE(PlatformerCameraTriggerEntity),
+
+		// sprites
+		(SpriteROMDef**)NULL,
+
+		// collision shapes
+		(ShapeDefinition*)this->platformerCameraTriggerEntityShapesDefinition,
+
+		// size
+		// if 0, width and height will be inferred from the first sprite's texture's size
+		{0, 0, 0},
+
+		// gameworld's character's type
+		focusEntityInGameType,
+
+		// physical specification
+		(PhysicalSpecification*)NULL,
+	};
+}
---
+++ cameras/Platformer/source/PlatformerCameraTriggerEntity/PlatformerCameraTriggerEntity.c
@@ -0,0 +1,211 @@
+/* VUEngine - Virtual Utopia Engine <http://vuengine.planetvb.com/>
+ * A universal game engine for the Nintendo Virtual Boy
+ *
+ * Copyright (C) 2007, 2018 by Jorge Eremiev <jorgech3@gmail.com> and Christian Radke <chris@vr32.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+ * NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+//---------------------------------------------------------------------------------------------------------
+//												INCLUDES
+//---------------------------------------------------------------------------------------------------------
+
+#include <PlatformerCameraTriggerEntity.h>
+#include <CollisionManager.h>
+#include <Optics.h>
+#include <Shape.h>
+#include <Camera.h>
+#include <InverseBox.h>
+#include <PlatformerLevelState.h>
+#include <PlatformerCameraMovementManager.h>
+
+
+//---------------------------------------------------------------------------------------------------------
+//												CLASS'S METHODS
+//---------------------------------------------------------------------------------------------------------
+
+// class's constructor
+void PlatformerCameraTriggerEntity::constructor(PlatformerCameraTriggerEntityDefinition* cameraEntityDefinition, s16 id, s16 internalId, const char* const name)
+{
+	ASSERT(cameraEntityDefinition, "PlatformerCameraTriggerEntity::constructor: null definition");
+
+	// construct base object
+	Base::constructor((EntityDefinition*)cameraEntityDefinition, id, internalId, name);
+
+	this->overridePositionFlag.x = false;
+	this->overridePositionFlag.y = false;
+	this->overridePositionFlag.z = false;
+
+	this->direction.x = __RIGHT;
+	this->direction.y = __DOWN;
+	this->direction.z = __FAR;
+
+	this->previousGlobalPosition = (Vector3D) {0, 0, 0};
+
+	PlatformerCameraTriggerEntity::lockMovement(this, __X_AXIS | __Y_AXIS, false);
+}
+
+// class's destructor
+void PlatformerCameraTriggerEntity::destructor()
+{
+	Camera::setFocusGameEntity(Camera::getInstance(), NULL);
+
+	// destroy the super object
+	// must always be called at the end of the destructor
+	Base::destructor();
+}
+
+void PlatformerCameraTriggerEntity::ready(bool recursive)
+{
+	Base::ready(this, recursive);
+
+	Entity::activateShapes(this, true);
+}
+
+void PlatformerCameraTriggerEntity::transform(const Transformation* environmentTransform, u8 invalidateTransformationFlag)
+{
+	Vector3D currentGlobalPosition = this->transformation.globalPosition;
+
+	Base::transform(this, environmentTransform, invalidateTransformationFlag);
+
+	if(this->overridePositionFlag.x)
+	{
+		this->transformation.globalPosition.x = currentGlobalPosition.x;
+	}
+
+	if(this->overridePositionFlag.y)
+	{
+		this->transformation.globalPosition.y = currentGlobalPosition.y;
+	}
+
+	Entity::transformShapes(this);
+
+//	Entity::showShapes(this);
+}
+
+void PlatformerCameraTriggerEntity::update(u32 elapsedTime)
+{
+	Base::update(this, elapsedTime);
+
+	// If there is a change in direction over the X axis
+	s8 currentXDirection = this->transformation.globalPosition.x > this->previousGlobalPosition.x ? __RIGHT : this->transformation.globalPosition.x < this->previousGlobalPosition.x ? __LEFT : 0;
+
+	if(currentXDirection)
+	{
+		if(currentXDirection != this->direction.x)
+		{
+			PlatformerCameraTriggerEntity::lockMovement(this, __X_AXIS, true);
+			this->direction.x = currentXDirection;
+		}
+
+		this->previousGlobalPosition = this->transformation.globalPosition;
+	}
+}
+
+void PlatformerCameraTriggerEntity::resume()
+{
+	Base::resume(this);
+
+	Camera::focus(Camera::getInstance(), false);
+
+	Vector3DFlag positionFlag = {true, true, true};
+	PlatformerCameraMovementManager::setPositionFlag(PlatformerCameraMovementManager::getInstance(), positionFlag);
+
+	PlatformerCameraTriggerEntity::lockMovement(this, __X_AXIS | __Y_AXIS, true);
+}
+
+void PlatformerCameraTriggerEntity::lockMovement(u8 axisToLockUp, bool locked)
+{
+	Vector3DFlag overridePositionFlag = PlatformerCameraTriggerEntity::getOverridePositionFlag(this);
+
+	Vector3DFlag positionFlag = PlatformerCameraMovementManager::getPositionFlag(PlatformerCameraMovementManager::getInstance());
+
+	if(__X_AXIS & axisToLockUp)
+	{
+		overridePositionFlag.x = locked;
+
+		positionFlag.x = !locked;
+	}
+
+	if(__Y_AXIS & axisToLockUp)
+	{
+		overridePositionFlag.y = locked;
+	}
+
+	PlatformerCameraTriggerEntity::setOverridePositionFlag(this, overridePositionFlag);
+	PlatformerCameraMovementManager::setPositionFlag(PlatformerCameraMovementManager::getInstance(), positionFlag);
+}
+
+void PlatformerCameraTriggerEntity::setOverridePositionFlag(Vector3DFlag overridePositionFlag)
+{
+	Container::invalidateGlobalPosition(this);
+
+	Transformation* environmentTransform = Container::getTransform(this->parent);
+
+	// don't lock yet, allow the global position to be calculated before locking
+	this->overridePositionFlag.y = false;
+	PlatformerCameraTriggerEntity::transform(this, environmentTransform, __INVALIDATE_TRANSFORMATION);
+
+	this->overridePositionFlag = overridePositionFlag;
+}
+
+Vector3DFlag PlatformerCameraTriggerEntity::getOverridePositionFlag()
+{
+	return this->overridePositionFlag;
+}
+
+// process collisions
+bool PlatformerCameraTriggerEntity::enterCollision(const CollisionInformation* collisionInformation)
+{
+	ASSERT(collisionInformation->collidingShape, "PlatformerCameraTriggerEntity::enterCollision: null collidingObjects");
+
+	Shape collidingShape = collisionInformation->collidingShape;
+	SpatialObject collidingObject = Shape::getOwner(collidingShape);
+
+#define CAMERA_BOUNDING_BOX_DISPLACEMENT	{__PIXELS_TO_METERS(0), __PIXELS_TO_METERS(-24/16), __PIXELS_TO_METERS(0)}
+
+	switch(SpatialObject::getInGameType(collidingObject))
+	{
+
+		case kNoType + 1:
+			{
+				if(collisionInformation->solutionVector.direction.y)
+				{
+					PlatformerCameraTriggerEntity::lockMovement(this, __Y_AXIS, false);
+				}
+
+				if(collisionInformation->solutionVector.direction.x)
+				{
+					PlatformerCameraTriggerEntity::lockMovement(this, __X_AXIS, false);
+				}
+				else
+				{
+					PlatformerCameraTriggerEntity::lockMovement(this, __Y_AXIS, false);
+				}
+
+				Vector3D position = CAMERA_BOUNDING_BOX_DISPLACEMENT;
+
+				Container::setLocalPosition(this, &position);
+			}
+			return false;
+			break;
+
+	}
+
+	return false;
+}
+
