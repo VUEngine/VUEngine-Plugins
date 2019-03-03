3cf64bcd2b4f4e3bb66597a110eed8fbc38ad4f3
Jorge Eremiev <jorgeche@Jorges-MacBook-Pro.local>
2019-03-03 12:46:26 -0500

    Started implementation of steering behaviors.

---
+++ steeringBehaviors/source/SteeringBehavior.c
@@ -0,0 +1,101 @@
+/* VUEngine - Virtual Utopia Engine <http://vuengine.planetvb.com/>
+ * A universal game engine for the Nintendo Virtual Boy
+ *
+ * Copyright (C) 2007, 2017 by Jorge Eremiev <jorgech3@gmail.com> and Christian Radke <chris@vr32.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+ * NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+//---------------------------------------------------------------------------------------------------------
+//												INCLUDES
+//---------------------------------------------------------------------------------------------------------
+
+#include "SteeringBehavior.h"
+
+
+//---------------------------------------------------------------------------------------------------------
+//												CLASS'S METHODS
+//---------------------------------------------------------------------------------------------------------
+
+/**
+ * Class constructor
+ */
+void SteeringBehavior::constructor()
+{
+	Base::constructor();
+}
+
+/**
+ * Class destructor
+ */
+void SteeringBehavior::destructor()
+{
+	// destroy the super object
+	// must always be called at the end of the destructor
+	Base::destructor();
+}
+
+static Vector3D SteeringBehavior::calculateForce(Vehicle vehicle)
+{
+	Vector3D steeringForce = {0, 0, 0};
+	
+	// otherwise it's just a velocity and add it
+	switch(Vehicle::getSummingMethod(vehicle)){
+
+		case kPrioritized:
+		
+			steeringForce = SteeringBehavior::calculatePrioritized(vehicle);
+			break;
+
+		case kWeightedAverage:
+		
+			steeringForce = SteeringBehavior::calculateWeightedSum(vehicle);
+			break;			
+	}
+
+	return steeringForce;
+}
+
+static Vector3D SteeringBehavior::calculatePrioritized(Vehicle vehicle)
+{
+	Vector3D steeringForce = {0, 0, 0};
+
+	SteeringBehavior* steeringBehaviors = Vehicle::getSteeringBehaviors(vehicle);
+	int i = 0;
+
+	for(; steeringBehaviors[i]; i++)
+	{
+
+	}
+
+	return steeringForce;
+}
+
+static Vector3D SteeringBehavior::calculateWeightedSum(Vehicle vehicle)
+{
+	Vector3D steeringForce = {0, 0, 0};
+
+	SteeringBehavior* steeringBehaviors = Vehicle::getSteeringBehaviors(vehicle);
+	int i = 0;
+
+	for(; steeringBehaviors[i]; i++)
+	{
+
+	}
+
+	return steeringForce;
+}
