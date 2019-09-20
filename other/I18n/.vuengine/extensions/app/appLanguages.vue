db2b8cf92502304f4762a07e472437a9a5d3738d
Christian Radke <christian.radke@unicepta.com>
2019-09-20 13:54:14 +0200

    Progress on i18n app

--- other/I18n/.vuengine/extensions/app/appLanguages.vue
+++ other/I18n/.vuengine/extensions/app/appLanguages.vue
@@ -43,20 +43,30 @@
 		}),
 		methods: {
 			onDataChanged() {
+				this.updateStrings()
+			},
+			updateStrings() {
 				let filteredStrings = [];
+
+				// get a sorted list of languages so the order of translations under "strings" won't depend on the order of languages
+				let languages = []
+				for (let language of this.data.config.project.i18n.languages) {
+					languages.push(language.id)
+				}
+				languages.sort()
 
 				for (let string of this.data.config.project.i18n.strings) {
 					let filteredString = {
 						"id": string.id,
 						"translations": []
 					};
-					for (let language of this.data.config.project.i18n.languages) {
+					for (let language of languages) {
 						let filteredTranslation = {
-							language: language.id,
+							language: language,
 							translation: ''
 						}
 						for (let translation of string.translations) {
-							if (translation.language == language.id) {
+							if (translation.language == language) {
 								filteredTranslation.translation = translation.translation
 							}
 						}
