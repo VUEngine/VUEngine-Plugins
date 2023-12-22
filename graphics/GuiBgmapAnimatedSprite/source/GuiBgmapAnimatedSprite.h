4fb93211f53ea9376f3cdf788c85a9c01abee434
Christian Radke <c.radke@posteo.de>
2023-12-22 12:05:07 +0100

    Align file headers

--- graphics/GuiBgmapAnimatedSprite/source/GuiBgmapAnimatedSprite.h
+++ graphics/GuiBgmapAnimatedSprite/source/GuiBgmapAnimatedSprite.h
@@ -1,7 +1,7 @@
 /**
  * VUEngine Plugins Library
  *
- * (c) Christian Radke and Jorge Eremiev
+ * © Jorge Eremiev <jorgech3@gmail.com> and Christian Radke <c.radke@posteo.de>
  *
  * For the full copyright and license information, please view the LICENSE file
  * that was distributed with this source code.
