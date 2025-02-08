bab3491d9127ae20649d5da576534630ce96f0a1
Christian Radke <c.radke@posteo.de>
2025-02-08 16:36:03 +0100

    Convert adjustment screens to use actor files

---
+++ states/splash/AdjustmentScreenPrototype/assets/Actor/Converted/AdjustmentScreenIconRActorSpec.c
@@ -0,0 +1,148 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////  THIS FILE WAS AUTO-GENERATED - DO NOT EDIT  ///////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+// INCLUDES
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+#include <Actor.h>
+#include <BgmapSprite.h>
+#include <InGameTypes.h>
+#include <Texture.h>
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+// DECLARATIONS
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+extern uint32 AdjustmentScreenIconRActorAdjustmentScreenIconTiles[];
+extern uint16 AdjustmentScreenIconRActorAdjustmentScreenIconMap[];
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+// SPRITES
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+CharSetROMSpec AdjustmentScreenIconRSprite1CharsetSpec =
+{
+	// Number of CHARs in function of the number of frames to load at the same time
+	4,
+
+	// Whether it is shared or not
+	true,
+
+	// Whether the tiles are optimized or not
+	true,
+
+	// Tiles array
+	AdjustmentScreenIconRActorAdjustmentScreenIconTiles,
+
+	// Frame offsets array
+	NULL
+};
+
+TextureROMSpec AdjustmentScreenIconRSprite1TextureSpec =
+{
+	// Pointer to the char spec that the texture uses
+	(CharSetSpec*)&AdjustmentScreenIconRSprite1CharsetSpec,
+
+	// Pointer to the map array that defines how to use the tiles from the char set
+	AdjustmentScreenIconRActorAdjustmentScreenIconMap,
+
+	// Horizontal size in tiles of the texture (max. 64)
+	2,
+
+	// Vertical size in tiles of the texture (max. 64)
+	2,
+
+	// padding for affine/hbias transformations
+	{0, 0},
+
+	// Number of frames that the texture supports
+	1,
+
+	// Palette index to use by the graphical data (0 - 3)
+	0,
+
+	// Flag to recyble the texture with a different map
+	false,
+
+	// Flag to vertically flip the image
+	false,
+
+	// Flag to horizontally flip the image
+	false
+};
+
+BgmapSpriteROMSpec AdjustmentScreenIconRSprite1SpriteSpec =
+{
+	{
+		// VisualComponent
+		{
+			// Component
+			{
+				// Allocator
+				__TYPE(BgmapSprite),
+
+				// Component type
+				kSpriteComponent
+			},
+
+			// Array of animation functions
+			(const AnimationFunction**)NULL
+		},
+
+		// Spec for the texture to display
+		(TextureSpec*)&AdjustmentScreenIconRSprite1TextureSpec,
+
+		// Transparency mode (__TRANSPARENCY_NONE, __TRANSPARENCY_EVEN or __TRANSPARENCY_ODD)
+		__TRANSPARENCY_NONE,
+
+		// Displacement added to the sprite's position
+		{0, 0, 0, 0}
+	},
+
+	// Flag to indicate in which display to show the texture (__WORLD_ON, __WORLD_LON or __WORLD_RON)
+	__WORLD_RON,
+
+	// The display mode (__WORLD_BGMAP, __WORLD_AFFINE, __WORLD_OBJECT or __WORLD_HBIAS)
+	__WORLD_BGMAP,
+	
+	// Pointer to affine/hbias manipulation function
+	NULL
+};
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+// ACTOR
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+ComponentSpec* const AdjustmentScreenIconRComponentSpecs[] = 
+{
+	(ComponentSpec*)&AdjustmentScreenIconRSprite1SpriteSpec,
+	NULL
+};
+
+ActorROMSpec AdjustmentScreenIconRActorSpec =
+{
+	// Class allocator
+	__TYPE(Actor),
+
+	// Component specs
+	(ComponentSpec**)AdjustmentScreenIconRComponentSpecs,
+
+	// Children specs
+	NULL,
+
+	// Extra info
+	NULL,
+
+	// Size
+	// If 0, it is computed from the visual components if any
+	{0, 0, 0},
+
+	// Actor's in-game type
+	kTypeNone,
+
+	// Animation to play automatically
+	NULL
+	
+};
---
+++ states/splash/AdjustmentScreenVUEngine/assets/Actor/Converted/AdjustmentScreenIconLActorSpec.c
@@ -0,0 +1,148 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////  THIS FILE WAS AUTO-GENERATED - DO NOT EDIT  ///////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+// INCLUDES
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+#include <Actor.h>
+#include <BgmapSprite.h>
+#include <InGameTypes.h>
+#include <Texture.h>
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+// DECLARATIONS
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+extern uint32 AdjustmentScreenIconLActorAdjustmentScreenIconTiles[];
+extern uint16 AdjustmentScreenIconLActorAdjustmentScreenIconMap[];
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+// SPRITES
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+CharSetROMSpec AdjustmentScreenIconLSprite1CharsetSpec =
+{
+	// Number of CHARs in function of the number of frames to load at the same time
+	8,
+
+	// Whether it is shared or not
+	true,
+
+	// Whether the tiles are optimized or not
+	true,
+
+	// Tiles array
+	AdjustmentScreenIconLActorAdjustmentScreenIconTiles,
+
+	// Frame offsets array
+	NULL
+};
+
+TextureROMSpec AdjustmentScreenIconLSprite1TextureSpec =
+{
+	// Pointer to the char spec that the texture uses
+	(CharSetSpec*)&AdjustmentScreenIconLSprite1CharsetSpec,
+
+	// Pointer to the map array that defines how to use the tiles from the char set
+	AdjustmentScreenIconLActorAdjustmentScreenIconMap,
+
+	// Horizontal size in tiles of the texture (max. 64)
+	3,
+
+	// Vertical size in tiles of the texture (max. 64)
+	3,
+
+	// padding for affine/hbias transformations
+	{0, 0},
+
+	// Number of frames that the texture supports
+	1,
+
+	// Palette index to use by the graphical data (0 - 3)
+	0,
+
+	// Flag to recyble the texture with a different map
+	false,
+
+	// Flag to vertically flip the image
+	false,
+
+	// Flag to horizontally flip the image
+	false
+};
+
+BgmapSpriteROMSpec AdjustmentScreenIconLSprite1SpriteSpec =
+{
+	{
+		// VisualComponent
+		{
+			// Component
+			{
+				// Allocator
+				__TYPE(BgmapSprite),
+
+				// Component type
+				kSpriteComponent
+			},
+
+			// Array of animation functions
+			(const AnimationFunction**)NULL
+		},
+
+		// Spec for the texture to display
+		(TextureSpec*)&AdjustmentScreenIconLSprite1TextureSpec,
+
+		// Transparency mode (__TRANSPARENCY_NONE, __TRANSPARENCY_EVEN or __TRANSPARENCY_ODD)
+		__TRANSPARENCY_NONE,
+
+		// Displacement added to the sprite's position
+		{0, 0, 0, 0}
+	},
+
+	// Flag to indicate in which display to show the texture (__WORLD_ON, __WORLD_LON or __WORLD_RON)
+	__WORLD_LON,
+
+	// The display mode (__WORLD_BGMAP, __WORLD_AFFINE, __WORLD_OBJECT or __WORLD_HBIAS)
+	__WORLD_BGMAP,
+	
+	// Pointer to affine/hbias manipulation function
+	NULL
+};
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+// ACTOR
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+ComponentSpec* const AdjustmentScreenIconLComponentSpecs[] = 
+{
+	(ComponentSpec*)&AdjustmentScreenIconLSprite1SpriteSpec,
+	NULL
+};
+
+ActorROMSpec AdjustmentScreenIconLActorSpec =
+{
+	// Class allocator
+	__TYPE(Actor),
+
+	// Component specs
+	(ComponentSpec**)AdjustmentScreenIconLComponentSpecs,
+
+	// Children specs
+	NULL,
+
+	// Extra info
+	NULL,
+
+	// Size
+	// If 0, it is computed from the visual components if any
+	{0, 0, 0},
+
+	// Actor's in-game type
+	kTypeNone,
+
+	// Animation to play automatically
+	NULL
+	
+};
